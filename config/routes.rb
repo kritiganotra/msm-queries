9b63def7d0e82e68330ca7916b2c71b930b36455
kritiganotra <kritiganotra@gmail.com>
2025-02-18 18:00:55 +0000

    Director index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/youngest", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "misc", :action => "homepage" })
+  get("/movies", { :controller => "misc", :action => "homepage" })
+  get("/actors", { :controller => "misc", :action => "homepage" })
 end
